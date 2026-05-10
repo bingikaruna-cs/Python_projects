b5e526fb9dbc10d014ff26821fdd8b0d582d4785
bingikaruna-cs <bingikaruna245@gmail.com>
2026-05-10 09:18:57 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("=== My First Python Calculator ===")
+print("Created by Karuna - BSc CS student")
+
+name=input("Enter your name: ")
+print("Hello",name,"! Welcome to my program")
+
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+print("Addition =",num1+num2)
+print("Subtraction=",num1-num2)
+print("Multiplication=",num1*num2)
+print("Thanks for using my first python code!")
